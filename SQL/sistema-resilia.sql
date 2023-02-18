ee5c6ae7be8f8289d0826a87e70f42b08444b705
dualsgo <contatombds@gmail.com>
2023-02-18 17:48:44 -0300

    Update sistema-resilia.sql

--- SQL/sistema-resilia.sql
+++ SQL/sistema-resilia.sql
@@ -1,4 +1,16 @@
 
+CREATE TABLE `Curso` (
+    `nome_curso` VARCHAR(255),
+    `Turno` VARCHAR(255),
+    `data_inicio` INT,
+    `data_termino` INT
+);
+
+CREATE TABLE `Turmas` (
+    `Facilitadores` VARCHAR(255),
+    `Turno` VARCHAR(255),
+    `Alunos` VARCHAR(255)
+);
 CREATE TABLE `Facilitadores` (
     `id` INT PRIMARY KEY,
     `nome` VARCHAR(255),
@@ -16,4 +28,4 @@ ALTER TABLE `Turmas` ADD FOREIGN KEY (`Turno`) REFERENCES `Curso` (`Turno`);
 
 ALTER TABLE `Turmas` ADD FOREIGN KEY (`Alunos`) REFERENCES `Alunos` (`id`);
 
-ALTER TABLE `Turmas` ADD FOREIGN KEY (`Facilitadores`) REFERENCES `Facilitadores` (`nome`)
+ALTER TABLE `Turmas` ADD FOREIGN KEY (`Facilitadores`) REFERENCES `Facilitadores` (`nome`);
